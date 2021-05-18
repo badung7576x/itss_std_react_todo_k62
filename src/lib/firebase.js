22b5d539472635edacd86c75f252e4e7db2ecfb0
badung7576x <Dangbadung7576@gmail.com>
2021-05-18 14:52:59 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrJk-oyw-egDrb_LINhwyz370rb5wWDSk",
+    authDomain: "serverless-app-dce27.firebaseapp.com",
+    projectId: "serverless-app-dce27",
+    storageBucket: "serverless-app-dce27.appspot.com",
+    messagingSenderId: "583032911419",
+    appId: "1:583032911419:web:b8ae939e3e3dd376e58e1e"
+}
+
+firebase.initializeApp(firebaseConfig);
